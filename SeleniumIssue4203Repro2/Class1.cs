cfcf736639ab7031de7d25280763c7e288dbae71
Roger Cook <roger@rogerdcook.com>
2017-06-30 20:38:22 -0500

    Add project files.

---
+++ SeleniumIssue4203Repro2/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumIssue4203Repro2
+{
+    public class Class1
+    {
+    }
+}
